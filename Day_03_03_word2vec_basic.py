c75207a990e97d17b21520cc1caf0cb2f9f9588c
Mincheol Shin <32638846+alsoj@users.noreply.github.com>
2019-03-19 08:36:42 +0900

    STYLE:Modified Formatting

--- Day_03_03_word2vec_basic.py
+++ Day_03_03_word2vec_basic.py
@@ -22,6 +22,31 @@ def show_dataset(tokens, window_size, is_skipgram):
 tokens = 'The quick brown fox jumps over the lazy dog'.split()
 print(tokens)
 
+show_dataset(tokens, 2, is_skipgram=True)
+# show_dataset(tokens, 2, is_skipgram=False)
 
-# show_dataset(tokens, 2, is_skipgram=True)
-show_dataset(tokens, 2, is_skipgram=False)
+#####################################################
+# show_dataset(tokens, 2, is_skipgram=True) 출력 결과
+#####################################################
+# ('The', 'quick') ('The', 'brown')
+# ('quick', 'The') ('quick', 'brown') ('quick', 'fox')
+# ('brown', 'The') ('brown', 'quick') ('brown', 'fox') ('brown', 'jumps')
+# ('fox', 'quick') ('fox', 'brown') ('fox', 'jumps') ('fox', 'over')
+# ('jumps', 'brown') ('jumps', 'fox') ('jumps', 'over') ('jumps', 'the')
+# ('over', 'fox') ('over', 'jumps') ('over', 'the') ('over', 'lazy')
+# ('the', 'jumps') ('the', 'over') ('the', 'lazy') ('the', 'dog')
+# ('lazy', 'over') ('lazy', 'the') ('lazy', 'dog')
+# ('dog', 'the') ('dog', 'lazy')
+
+#####################################################
+# show_dataset(tokens, 2, is_skipgram=False) 출력 결과
+#####################################################
+# ['quick', 'brown'] The
+# ['The', 'brown', 'fox'] quick
+# ['The', 'quick', 'fox', 'jumps'] brown
+# ['quick', 'brown', 'jumps', 'over'] fox
+# ['brown', 'fox', 'over', 'the'] jumps
+# ['fox', 'jumps', 'the', 'lazy'] over
+# ['jumps', 'over', 'lazy', 'dog'] the
+# ['over', 'the', 'dog'] lazy
+# ['the', 'lazy'] dog
