551f8cfc4c5303d1eae2a3125e0491a563755ea8
Mincheol Shin <32638846+alsoj@users.noreply.github.com>
2019-03-19 07:51:50 +0900

    STYLE:Modified Formatting

--- Day_02_03_comprehension.py
+++ Day_02_03_comprehension.py
@@ -1,8 +1,9 @@
 # Day_02_03_comprehension.py
 import random
 
-# 컴프리헨션 : 콜렉션을 만드는 한 줄짜리 반복문
-
+################################################
+# Comprehension : 콜렉션을 만드는 한 줄짜리 반복문
+################################################
 for i in range(5):
     print(i, end=' ')
 
@@ -31,9 +32,13 @@ print()
 # 난수 10개가 들어있는 리스트를 만드세요
 print([random.randrange(100) for _ in range(10)])        # 난수 리스트 컴프리헨션
 
+
+# 문제
 # 홀수 난수 10개가 들어있는 리스트를 만드세요
 print([random.randrange(1, 100, 2) for _ in range(10)])        # 홀수 난수 리스트 컴프리헨션
 
+
+# 문제
 # 리스트에 들어있는 홀수만 출력하세요
 t = [random.randrange(100) for _ in range(10)]
 
@@ -45,15 +50,15 @@ print()
 # 컴프리헨션은 for 문이 구성될 수 있는 것들로만 가능하다, 재구성
 print(sum([i for i in t if i % 2]))
 
-
 print('/' * 50)
-
 
 t1 = [random.randrange(100) for _ in range(10)]
 t2 = [random.randrange(100) for _ in range(10)]
 t3 = [random.randrange(100) for _ in range(10)]
 
-# 2차원 배열
+##########################
+# 2차원 배열 Comprehension
+##########################
 all = [t1, t2, t3]
 
 print([i for i in all])
@@ -80,7 +85,9 @@ print([c for c in 'tensor'])
 print(list('Tensor'.lower()))
 print([c.lower() for c in 'Tensor'])
 
+#########################################################################
 # zip : 여러개의 컬렉션을 묶어주는 역할을 한다(하나씩 가져와서 tuple로 묶어줌)
+#########################################################################
 print([c for c in zip('tensor', range(6))])
 print([c for c, i in zip('tensor', range(6))])
 print([(c, i) for c, i in zip('tensor', range(6))])
